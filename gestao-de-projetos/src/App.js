c061f9f939a0705ea90636e83cd8f1a0722477cb
Heber Silverio <hebersilverio@gmail.com>
2022-08-12 14:47:44 -0300

    Entendendo o JSX

--- gestao-de-projetos/src/App.js
+++ gestao-de-projetos/src/App.js
@@ -2,10 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Héber'
+  const newName = name.toUpperCase()
+
+  function sum(a, b) {
+    return a + b
+  }
+
+  const url = "https://via.placeholder.com/150"
+
   return (
     <div className="App">
       <h1>Olá React</h1>
       <p>Meu primeiro aplicativo em React</p>
+      <h2>Alterando o JSX</h2>
+      <p>Olá, {name}! Você gostaria de ter o nome sempre em maiúsculas, assim: {newName}? </p>
+      <p>Soma: {2 + 2}</p>
+      <p>Retorno da função soma: {sum(1, 2)}</p>
+      <img src={url} alt="Minha Imagem" />
+      
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
