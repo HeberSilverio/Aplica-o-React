a39295761081050a18193b0286a5bd56728cfe84
Heber Silverio <hebersilverio@gmail.com>
2022-08-18 22:55:04 -0300

    Adiciona css ao projeto

--- gestao-de-projetos/src/App.js
+++ gestao-de-projetos/src/App.js
@@ -2,19 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
    const clienteNome = 'Isaque'
 
     return (
-    <div className="App">
-      
-      <SayMyName nome="Matheus" />
-      <SayMyName nome="Héber" />
-      <SayMyName nome={clienteNome} />
-      <Pessoa nome="Rodrigo" idade="36" Profissao="Programador" foto="https://via.placeholder.com/150" />
-      
-    </div>
+      <header>
+         <div className="App">
+            <ul class="nav">
+               <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Active</a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+               </li>
+            </ul>
+            <Frase />
+         </div>
+         <div className="PrimeiroApp">
+            <SayMyName nome="Matheus" />
+            <SayMyName nome="Héber" />
+            <SayMyName nome={clienteNome} />
+            <Pessoa nome="Rodrigo" idade="36" Profissao="Programador" foto="https://via.placeholder.com/150" />
+            
+         </div>
+    </header>
   );
 }
 
--- gestao-de-projetos/src/components/Frase.js
+++ gestao-de-projetos/src/components/Frase.js
@@ -1,9 +1,19 @@
+import styles from './Frase.module.css'
+
 function Frase() {
    return (
-      <div>
-         <p>Este é um componente com uma frase!</p>
-      </div>
+      <section>
+               
+         <div className={styles.fraseContainer}>
+            <p className="alert alert-primary mb-2 mb-lg-0" role="alert">Este é um componente com uma frase!</p>
+         </div>
+         <div className="alert alert-primary " role="alert">
+         A simple primary alert—check it out!
+         </div>
+    </section>
    )
 }
 
 export default Frase
+
+{/* {styles.fraseContent} */}
